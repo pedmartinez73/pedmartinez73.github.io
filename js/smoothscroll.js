a129fba01628698ebeef52672994fa875107029a
pedmartinez73 <pedrmartinez@gmail.com>
2015-05-21 11:53:48 -0400

    hh

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -19,26 +19,6 @@ $(".navbar-collapse ul li a[href^='#']").on('click', function(e) {
 
 });
 
-$(".navbar-collapse ul li a[href^='#team']").on('click', function(e) {
-
-   // prevent default anchor click behavior
-   e.preventDefault();
-
-   // store hash
-   var hash = this.hash;
-
-   // animate
-   $('html, body').animate({
-       scrollTop: $(this.hash).offset().top -0
-     }, 700, function(){
-
-       // when done, add hash to url
-       // (default click behaviour)
-       window.location.hash = hash;
-     });
-
-});
-
 
 $("a.navbar-brand[href^='#']").on('click', function(e) {
 
