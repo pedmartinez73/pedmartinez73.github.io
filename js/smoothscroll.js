5d79c5f695bf4f97824b1d5f48459e5e71471653
pedmartinez73 <pedrmartinez@gmail.com>
2015-05-21 11:33:46 -0400

    y

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,23 +1,4 @@
 
-$(".navbar-collapse ul li a[href^='#team']").on('click', function(e) {
-
-   // prevent default anchor click behavior
-   e.preventDefault();
-
-   // store hash
-   var hash = this.hash;
-
-   // animate
-   $('html, body').animate({
-       scrollTop: $(this.hash).offset().top -270
-     }, 700, function(){
-
-       // when done, add hash to url
-       // (default click behaviour)
-       window.location.hash = hash;
-     });
-
-});
 $(".navbar-collapse ul li a[href^='#']").on('click', function(e) {
 
    // prevent default anchor click behavior
