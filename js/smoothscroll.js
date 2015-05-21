a57653a239fcec4cbc07c3a105e5ccc561057200
pedmartinez73 <pedrmartinez@gmail.com>
2015-05-21 11:09:59 -0400

    team fix

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -37,3 +37,23 @@ $("a.navbar-brand[href^='#']").on('click', function(e) {
      });
 
 });
+
+$(".navbar-collapse ul li a[href^='#team']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top -100
+     }, 700, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
